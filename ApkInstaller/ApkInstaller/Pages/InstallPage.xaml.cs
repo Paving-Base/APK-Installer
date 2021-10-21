bac0659996c9ede581158d8b10dc9a7bc07fc825
wherewhere <qq2518766683@outlook.com>
2021-10-21 22:43:29 +0800

    现在基本可以使用了

--- ApkInstaller/ApkInstaller/Pages/InstallPage.xaml.cs
+++ ApkInstaller/ApkInstaller/Pages/InstallPage.xaml.cs
@@ -7,10 +7,12 @@ using Microsoft.UI.Xaml.Navigation;
 using Microsoft.Windows.AppLifecycle;
 using SharpAdbClient;
 using SharpAdbClient.DeviceCommands;
+using SharpAdbClient.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
 
@@ -24,7 +26,8 @@ namespace APKInstaller.Pages
     /// </summary>
     public sealed partial class InstallPage : Page, INotifyPropertyChanged
     {
-        private string path;
+        private string path/*= @"C:\Users\qq251\Downloads\Programs\Coolapk-11.4.3-2110131-coolapk-app-sign.apk"*/;
+        private DeviceData device;
 
         private ApkInfo _apkInfo = null;
         internal ApkInfo ApkInfo
@@ -92,6 +95,7 @@ namespace APKInstaller.Pages
         {
             if (!IsInstalling)
             {
+                new AdbClient().Connect(new DnsEndPoint("127.0.0.1", 58526));
                 DispatcherQueue.TryEnqueue(() => CheckAPK());
             }
         }
@@ -101,7 +105,11 @@ namespace APKInstaller.Pages
             if (!string.IsNullOrEmpty(path))
             {
                 WaitProgressText.Text = "Starting ADB Server...";
-                await Task.Run(() => { new AdbServer().StartServer(@$"{AppDomain.CurrentDomain.BaseDirectory}\platform-tools\adb.exe", restartServerIfNewer: false); });
+                await Task.Run(() =>
+                {
+                    new AdbServer().StartServer(@$"{AppDomain.CurrentDomain.BaseDirectory}\platform-tools\adb.exe", restartServerIfNewer: false);
+                    new AdbClient().Connect(new DnsEndPoint("127.0.0.1", 58526));
+                });
                 WaitProgressText.Text = "Loading...";
                 ApkInfo = await Task.Run(() =>
                 {
@@ -109,12 +117,26 @@ namespace APKInstaller.Pages
                     return AAPTool.Decompile(path);
                 });
                 WaitProgressText.Text = "Checking...";
-                await Task.Run(() => DispatcherQueue.TryEnqueue(() => CheckAPK()));
+                if (CheckDevice() && device != null)
+                {
+                    CheckAPK();
+                }
+                else
+                {
+                    ActionButton.IsEnabled = false;
+                    ActionButtonText.Text = "Install";
+                    InfoMessageTextBlock.Text = "Waiting for Device...";
+                    AppName.Text = $"Waiting for install {ApkInfo.AppName}";
+                    ActionButton.Visibility = MessagesToUserContainer.Visibility = Visibility.Visible;
+                }
                 WaitProgressText.Text = "Finished";
             }
             else
             {
-                ShowError("Failed to locate the app package because the URI is invalid.");
+                ResetUI();
+                ApkInfo = new ApkInfo();
+                AppName.Text = "For proper functioning of the app, try to launch a Android app package.";
+                AppVersion.Visibility = AppPublisher.Visibility = AppCapabilities.Visibility = Visibility.Collapsed;
             }
             IsInitialized = true;
         }
@@ -144,43 +166,94 @@ namespace APKInstaller.Pages
             AppVersion.Visibility = AppPublisher.Visibility = AppCapabilities.Visibility = Visibility.Collapsed;
         }
 
+        private bool CheckDevice()
+        {
+            List<DeviceData> devices = new AdbClient().GetDevices();
+            if (devices.Count <= 0) { return false; }
+            foreach (DeviceData device in devices)
+            {
+                if(device.Model.Contains("Subsystem_for_Android_TM_"))
+                {
+                    this.device = device ?? this.device;
+                    return true;
+                }
+            }
+            return false;
+        }
+
         private void CheckAPK()
         {
             ResetUI();
             AdbClient client = new AdbClient();
-            if (client.GetDevices() is List<DeviceData> devices && devices.Count > 0)
+            //if (client.GetDevices() is List<DeviceData> devices && devices.Count > 0)
+            //{
+            //DeviceData device = client.GetDevices().First();
+            if(device == null) { return; }
+            PackageManager manager = new PackageManager(client, device);
+            var info = manager.GetVersionInfo(ApkInfo.PackageName);
+            if (info == null)
             {
-                DeviceData device = client.GetDevices().First();
-                PackageManager manager = new PackageManager(client, device);
-                var info = manager.GetVersionInfo(ApkInfo.PackageName);
-                if (info == null)
-                {
-                    ActionButtonText.Text = "Install";
-                    AppName.Text = $"Install {ApkInfo.AppName}?";
-                    ActionButton.Visibility = LaunchWhenReadyCheckbox.Visibility = Visibility.Visible;
-                }
-                else if (info.VersionCode < int.Parse(ApkInfo.VersionCode))
-                {
-                    ActionButtonText.Text = "Update";
-                    AppName.Text = $"Update {ApkInfo.AppName}?";
-                    ActionButton.Visibility = LaunchWhenReadyCheckbox.Visibility = Visibility.Visible;
-                }
-                else
-                {
-                    ActionButtonText.Text = "Reinstall";
-                    SecondaryActionButtonText.Text = "Close";
-                    AppName.Text = $"{ApkInfo.AppName} is already installed";
-                    TextOutput.Text = $"A newer version of {ApkInfo.AppName} is already installed.";
-                    ActionButton.Visibility = SecondaryActionButton.Visibility = TextOutputScrollViewer.Visibility = Visibility.Visible;
-                }
+                ActionButtonText.Text = "Install";
+                AppName.Text = $"Install {ApkInfo.AppName}?";
+                ActionButton.Visibility = LaunchWhenReadyCheckbox.Visibility = Visibility.Visible;
+            }
+            else if (info.VersionCode < int.Parse(ApkInfo.VersionCode))
+            {
+                ActionButtonText.Text = "Update";
+                AppName.Text = $"Update {ApkInfo.AppName}?";
+                ActionButton.Visibility = LaunchWhenReadyCheckbox.Visibility = Visibility.Visible;
             }
             else
             {
-                ActionButton.IsEnabled = false;
-                ActionButtonText.Text = "Install";
-                InfoMessageTextBlock.Text = "Waiting for Device...";
-                AppName.Text = $"Waiting for install {ApkInfo.AppName}";
-                ActionButton.Visibility = MessagesToUserContainer.Visibility = Visibility.Visible;
+                ActionButtonText.Text = "Reinstall";
+                SecondaryActionButtonText.Text = "Launch";
+                AppName.Text = $"{ApkInfo.AppName} is already installed";
+                TextOutput.Text = $"A newer version of {ApkInfo.AppName} is already installed.";
+                ActionButton.Visibility = SecondaryActionButton.Visibility = TextOutputScrollViewer.Visibility = Visibility.Visible;
+            }
+            //}
+            //else
+            //{
+            //    ActionButton.IsEnabled = false;
+            //    ActionButtonText.Text = "Install";
+            //    InfoMessageTextBlock.Text = "Waiting for Device...";
+            //    AppName.Text = $"Waiting for install {ApkInfo.AppName}";
+            //    ActionButton.Visibility = MessagesToUserContainer.Visibility = Visibility.Visible;
+            //}
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch((sender as FrameworkElement).Name)
+            {
+                case "ActionButton":
+                    InstallAPP();
+                    break;
+            }
+        }
+
+        private async void InstallAPP()
+        {
+            try
+            {
+                IsInstalling = true;
+                CancelOperationButtonText.Text = "Cancel";
+                CancelOperationButton.Visibility = Visibility.Visible;
+                ActionButton.Visibility = SecondaryActionButton.Visibility = Visibility.Collapsed;
+                await Task.Run(() =>
+                {
+                    new PackageManager(new AdbClient(), device).InstallPackage(path, true);
+                });
+                IsInstalling = false;
+                SecondaryActionButtonText.Text = "Launch";
+                SecondaryActionButton.Visibility = Visibility.Visible;
+                CancelOperationButton.Visibility = LaunchWhenReadyCheckbox.Visibility = Visibility.Collapsed;
+            }
+            catch (AdbException ex)
+            {
+                TextOutput.Text = ex.Message;
+                TextOutputScrollViewer.Visibility = InstallOutputTextBlock.Visibility = Visibility.Visible;
+                ActionButton.Visibility = SecondaryActionButton.Visibility = CancelOperationButton.Visibility = Visibility.Collapsed;
             }
         }
     }
